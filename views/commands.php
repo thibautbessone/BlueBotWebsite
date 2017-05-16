a2b96661a0dcad88f0e1ffc86bb808002621b3ca
thibautbessone <thibaut13b@gmail.com>
2017-05-16 20:28:40 +0200

    Added commands and design adjusts

--- views/commands.php
+++ views/commands.php
@@ -4,14 +4,22 @@ Version : 0.3
 -->
 
 <div id="commandsPage">
+    <div id="index-banner" class="parallax-container parallaxCommands">
+        <div class="section no-pad-bot">
+            <div class="container">
+                <h4 class="header center blue-text text-darken-2">BlueBot Commands</h4>
+                <div class="row center">
+                    <h5 class="header col s12 light">All commands are prefixed with a ! by default. However, this prefix is customizable.</h5>
+                    <h5 class="header col s12 light">Help for each command is available by typing !command help.</h5>
+                </div>
+            </div>
+        </div>
+        <div class="parallax"><img id="parallaxBg" src="img/code.png" alt="code background"></div>
+    </div>
     <div class="container">
         <div class="section">
             <div class="row">
                 <div class="col s12 center">
-                    <h3><i class="mdi-content-send brown-text"></i></h3>
-                    <h4>BlueBot Commands</h4>
-                    <p>All commands are prefixed with a ! by default. However, this prefix is customizable.</p>
-                    <p>Help for each command is available by typing !command help .</p>
                     <?php
                     include 'commands/utility.html';
                     include 'commands/moderation.html';

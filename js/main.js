4e24ce19dfd40d6b1d94226cc139e2c7a6c2c33d
thibautbessone <thibaut13b@gmail.com>
2017-05-18 17:56:51 +0200

    Fixed little mistakes

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
 
     //$(".button-collapse").sideNav();
     $('.button-collapse').sideNav({
-        menuWidth: 150,
+        menuWidth: 160,
         edge: 'left',
         closeOnClick: true,
         draggable: true
